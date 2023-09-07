a1bf7d804a829ee46c297b1dff8d9f633d3ae27e
Alejandra Santos <49631442+Alevsdl@users.noreply.github.com>
2023-09-07 02:14:26 -0600

    all auth

---
+++ app/Filament/Resources/RoleResource.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\RoleResource\Pages;
+use App\Filament\Resources\RoleResource\RelationManagers;
+use Spatie\Permission\Models\Role;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\TextColumn;
+
+class RoleResource extends Resource
+{
+    protected static ?string $model = Role::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-cog';
+    protected static ?string $modelLabel = 'Roles ';
+    protected static ?string $navigationGroup = 'Gestión de Administrador ';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+                //
+                Card::make()
+                    ->schema([
+                        TextInput::make('name')->unique(ignoreRecord: true)->required()->label('Nombre'),
+                        Select::make('permissions')->multiple()->relationship('permissions', 'name')->preload()->required()
+
+                    ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+                TextColumn::make('id')->sortable(),
+                TextColumn::make('name')->sortable()->searchable()->label('Nombre'),
+                TextColumn::make('created_at')->sortable()->searchable()->dateTime('d-M-Y')->label('Creación'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make()
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListRoles::route('/'),
+            'create' => Pages\CreateRole::route('/create'),
+            'edit' => Pages\EditRole::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Filament\Resources\UserResource\RelationManagers\RolesRelationManager;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\CheckboxList;
+use Illuminate\Support\Facades\Hash;
+
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-users';
+    protected static ?string $modelLabel = 'Usuarios ';
+    protected static ?string $navigationGroup = 'Gestión de Administrador ';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                ->label('Nombre')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Toggle::make('is_admin')
+                ->label('Privilegios de administrator')
+                    ->required(),
+                Forms\Components\TextInput::make('email')
+                ->label('Email')
+                    ->email()
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('password')
+                    ->password()
+                    ->required()
+                    ->maxLength(255),
+                CheckboxList::make('roles')->relationship('roles', 'name')->columns(2)->helperText('Choose only one role')->required()
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')->label('Nombre'),
+                Tables\Columns\IconColumn::make('is_admin')->label('Admin')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('email'),
+
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make()
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+                //
+            RolesRelationManager::class,
+        ];
+    }
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
