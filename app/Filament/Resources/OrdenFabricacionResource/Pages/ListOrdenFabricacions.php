87bf10281a495284005a34b1221c9d910232092e
Alejandra Santos <49631442+Alevsdl@users.noreply.github.com>
2023-10-26 11:18:00 -0600

    detalleee

--- app/Filament/Resources/OrdenFabricacionResource/Pages/ListOrdenFabricacions.php
+++ app/Filament/Resources/OrdenFabricacionResource/Pages/ListOrdenFabricacions.php
@@ -13,7 +13,7 @@ class ListOrdenFabricacions extends ListRecords
     protected function getActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            //Actions\CreateAction::make(),
         ];
     }
 }
