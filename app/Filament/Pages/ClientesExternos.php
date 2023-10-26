47e291c6c541c0fe6fd1ecbf9c1a53b9320f8f8e
Alejandra Santos <49631442+Alevsdl@users.noreply.github.com>
2023-10-26 12:03:03 -0600

    aaaaaa

---
+++ app/Filament/Pages/ClientesExternos.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+
+class ClientesExternos extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.clientes-externos';
+
+
+}
--- app/Http/Livewire/VistaExterna.php
+++ app/Http/Livewire/VistaExterna.php
@@ -3,12 +3,42 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\PedidoMinorista;
 
 class VistaExterna extends Component
 {
+    public $id_producto;
+    public $cantidad;
+    public $tienda;
+    public $fecha;
+
 
     public function mount()
     {
         return view('livewire.vista-externa');
     }
+
+    public function savePedido(){
+        try{
+            PedidoMinorista::create([
+                    'id_producto'=> $this->id_producto,
+                    'cantidad'=> $this->cantidad,
+                    'total'=> 0,
+                    'tienda'=> $this->tienda,
+                    'fecha'=> $this->fecha,
+                    'procesado'=> false
+
+            ]);
+
+        } catch(Exception $e){
+            error_log($e->getMessage());
+        }
+
+        error_log($this->id_producto);
+        error_log($this->cantidad);
+        error_log($this->tienda);
+        error_log($this->fecha);
+    }
+
+
 }
